2c1e4e2d7da556556e833fadf007507416bbffd8
Matthew Perkins <mfperkins@gmail.com>
2017-04-07 14:04:06 +0100

    adding in state to AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,19 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List !!!!</Text>
+        <Text> this.state.albums </Text>
       </View>
     );
   }
