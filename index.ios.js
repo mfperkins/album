a43564dac604f03b548c495bfbd6522989c66e99
Matthew Perkins <mfperkins@gmail.com>
2017-04-07 15:42:12 +0100

    scrolling card list

--- index.ios.js
+++ index.ios.js
@@ -1,21 +1,13 @@
-// Index.ios.js - place code in here for iOS!
-
-// 1) Import a library to help create a component
-
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
-// 2) Create a component
-
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
 );
-
-// 3) Render it to the device
 
 AppRegistry.registerComponent('albums', () => App);
