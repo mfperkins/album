84701b561d3509ee7f9e7d8529eba317822d7144
Matthew Perkins <mfperkins@gmail.com>
2017-04-07 16:24:45 +0100

    changing button message

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, buttonTextStyle } = styles;
 
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={buttonTextStyle}>
-        Click me!!
+        { children }
       </Text>
     </TouchableOpacity>
   );
