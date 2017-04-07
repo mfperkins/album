ba531d58dcafbc7daf3ecf6b213f2e77144ff0b0
Matthew Perkins <mfperkins@gmail.com>
2017-04-07 15:34:27 +0100

    adding in images

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,22 +4,34 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, cardHeaderStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    cardHeaderStyle,
+    thumbnailContainerStyle,
+    cardHeaderTextStyle,
+    imageStyle
+    } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             source={{ uri: thumbnail_image }}
             style={thumbnailStyle}
           />
         </View>
         <View style={cardHeaderStyle}>
-          <Text>{title}</Text>
+          <Text style={cardHeaderTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>
   );
@@ -30,9 +42,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  cardHeaderTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 0,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
